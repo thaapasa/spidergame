f07fc4b1c34e2bccfcd5a6cd090ae9c925ccb4b1
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-12-30 18:03:30 +0200

    Configure typescript and linter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,13 +8,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>jAlkaMetri</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
