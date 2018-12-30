acb6e9a02403792ae40f2c51ec165c338feaf9a8
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2018-12-30 18:40:42 +0200

    Add intro image

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Image, StyleSheet, View } from 'react-native';
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Täditikki Tedi</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image
+          source={require('../assets/img/intro-bg.jpg')}
+          resizeMode="contain"
+          style={styles.image}
+        />
       </View>
     );
   }
@@ -21,19 +17,18 @@ export default class App extends Component<{}> {
 
 const styles = StyleSheet.create({
   container: {
+    alignSelf: 'stretch',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 0,
+    margin: 0,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  image: {
+    width: '100%',
+    height: '100%',
+    padding: 0,
+    margin: 0,
+    backgroundColor: 'black',
   },
 });
