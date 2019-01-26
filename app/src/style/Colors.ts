adaa328e755070ccf6329ef4d4ed3f95468c85dd
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-01-26 21:25:49 +0200

    Add more colors

--- app/src/style/Colors.ts
+++ app/src/style/Colors.ts
@@ -3,10 +3,12 @@ export const Colors = {
   lightGrey: '#B5BCBB',
   darkGrey: '#53504C',
   purple: '#A65198',
+  darkBlue: '#3297FD',
   lightBlue: '#8FCAF9',
   blue: '#4AA9F5',
   yellow: '#F9CF1D',
   red: '#DA4D65',
   green: '#2A9A4E',
+  lightGreen: '#49C477',
   black: '#303030',
 };
